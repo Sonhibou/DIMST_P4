897772ec1eb3d8f28a3c323c1bc3423cf03ca14d
Thierno omar diouf <thiernoomardiouf@gmail.com>
2021-10-12 16:52:49 +0000

    Creation du fichier afficher.php

---
+++ afficher.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page de traitement</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p>Bonjour et bienvenue sur notre plateforme. vous ètes inscris avec ces données :</p>
+        
+        <?php
+            echo 'Prénom : '.$_POST["prenom"].'<br>';
+            echo 'Nom : ' .$_POST["nom"].'<br>';
+            echo 'Email : ' .$_POST["mail"].'<br>';
+            echo 'Avec comme mot de passe : ' .$_POST["password"].'<br>';
+        ?>
+      
+
+    </body>
+    <p>
+        
+    </p>
+</html>
